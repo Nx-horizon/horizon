8d7db161a2533f421fbaa36545f086d8ae465c9f
Cameleon00722 <45.worker@protonmail.com>
2023-12-30 20:58:50 +0100

    kdf enhanced

--- src/kdfwagen.rs
+++ src/kdfwagen.rs
@@ -31,16 +31,17 @@ fn hmac(key: &[u8], message: &[u8]) -> Vec<u8> {
     // Concaténer le masque externe avec le haché interne
     let outer_input: Vec<u8> = opad.into_iter().chain(inner_hash.iter().cloned()).collect();
 
-    // Appliquer le hachage externe (SHA-256) pour obtenir la sortie finale
+    // Appliquer le hachage externe (SHA-512) pour obtenir la sortie finale
     Sha3_512::digest(outer_input).to_vec()
 }
 
 // Fonction PBKDF2
-fn kdfwagen(password: &[u8], salt: &[u8], iterations: usize, key_length: usize) -> Vec<u8> {
+fn kdfwagen(password: &[u8], salt: &[u8], iterations: usize) -> Vec<u8> {
     const PRF_OUTPUT_SIZE: usize = 64; // Taille de sortie de la fonction de hachage utilisée (SHA-512)
+    const KEY_LENGTH: usize = 512; // Taille de la clé de sortie
 
     let mut result = Vec::new();
-    let mut block_count = (key_length + PRF_OUTPUT_SIZE - 1) / PRF_OUTPUT_SIZE;
+    let mut block_count = (KEY_LENGTH + PRF_OUTPUT_SIZE - 1) / PRF_OUTPUT_SIZE;
 
     if block_count > 255 {
         block_count = 255; // Limiter le nombre de blocs pour éviter le débordement
@@ -58,10 +59,10 @@ fn kdfwagen(password: &[u8], salt: &[u8], iterations: usize, key_length: usize) 
             xor_result.iter_mut().zip(u.iter()).for_each(|(a, b)| *a ^= b);
         }
 
-        result.extend_from_slice(&xor_result[..std::cmp::min(PRF_OUTPUT_SIZE, key_length)]);
+        result.extend_from_slice(&xor_result[..std::cmp::min(PRF_OUTPUT_SIZE, KEY_LENGTH)]);
     }
 
-    result.resize(key_length, 0);
+    result.resize(KEY_LENGTH, 0);
     result
 }
 
@@ -84,9 +85,8 @@ mod tests {
         let password = b"password";
         let salt = b"salt";
         let iterations = 2;
-        let key_length = 32;
-        let expected = "4714bcc31299ffafd9bef3315f28596a77fc51e5dca321e485dbf67e203f7c5c";
-        let result = kdfwagen(password, salt, iterations, key_length);
+        let expected = "4714bcc31299ffafd9bef3315f28596a77fc51e5dca321e485dbf67e203f7c5c08a01f65090539ff9a0ae2d610373729ed3e7f54201d581a7e9a2d023b6900407c9aa051f645cb7254772d3f538aa1714ec81b10204e2b70c1b9c7f118e4713cd2e17ce7d6f450fadfdd40727001e16554a58ac16c589feaba2f7c58827b517d00416e457e494721b5f3d150fd76d91ca5a0034da249b23d9cc07cfc5916ce568c24ac15bc936b6f26c3f75625146e9927113adefc35eb8a4ee5f4c18ccc2cba7efef4510307f8ccd0f8d523cbf3efbb26da6cd7aaf31eb541b603d6e1ab07e2eb8d440690e9fb8ba48a470257c95b76047af1b696d87ca78d8b91743e6a3ed0e335285f248729b644fe1eeb07487daedc04581b244e149eeb0bc4c98f7a323ac805141741992b88b7d6586cce599e508f6a7581f6589739f68079f0f158519039f2cb05e302b953b324ff7d52993d5cb8b8ddc5793f9bfaa27cfae49465e9f0ef8938aefb94a96cb48697f3db737a96d9a0c8c1b2bc3975f0aea3e3fe0582bb1456cbadab03466c9fa5d91c796db5304258350f3bc1be5cea14dff983b48c12bac1f20327b55a3215b91506db3c6fdad0a58253405a033b0d68a3145f5e91b2f54f9ca7dfe864e680b5af70545aed4553a9f180025fc98d713e5d4408bf2e65dcbc94e1acd9a5d154e04f26fcc2617ee0acb5cc164280c6e504b291635fcd5b";
+        let result = kdfwagen(password, salt, iterations);
         assert_eq!(hex::encode(result), expected);
     }
 }
