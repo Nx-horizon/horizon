8b0e1eeda06b8186cca8e03930b5b4eeedf35e3b
Pratalmox <130173781+Pratalmox@users.noreply.github.com>
2024-03-06 09:23:17 +0100

    Update cryptex.rs
    add documentation

--- src/cryptex.rs
+++ src/cryptex.rs
@@ -5,6 +5,51 @@ use secrecy::{ExposeSecret, Secret};
 use crate::{addition_chiffres, get_salt, KEY_LENGTH, nebula, NUM_ITERATIONS, shift_bits, table3, unshift_bits, vz_maker, xor_crypt3};
 use crate::kdfwagen::kdfwagen;
 
+
+/// This function encrypts the content of a file using two secret keys and a password.
+///
+/// # Arguments
+///
+/// * `plain_text` - The content of the file to be encrypted.
+/// * `key1` - A secret key used for encryption.
+/// * `key2` - Another secret key used for encryption.
+/// * `password` - The password used for additional encryption.
+///
+/// # Returns
+///
+/// A `Result` containing the encrypted content of the file, or an error if encryption fails.
+///
+/// # Errors
+///
+/// Returns an error if encryption fails for any reason.
+///
+/// # Example
+///
+/// ```
+/// use your_crate::encrypt_file;
+///
+/// // Read the content of the file to be encrypted
+/// let plain_text = std::fs::read("file.txt").expect("Failed to read file");
+///
+/// // Provide secret keys and a password
+/// let key1 = Secret::new(vec![1, 2, 3]);
+/// let key2 = Secret::new(vec![4, 5, 6]);
+/// let password = "my_password";
+///
+/// // Encrypt the content of the file
+/// let encrypted = encrypt_file(plain_text, &key1, &key2, password);
+///
+/// match encrypted {
+///     Ok(encrypted_content) => {
+///         // Write the encrypted content to another file
+///         std::fs::write("encrypted_file.txt", encrypted_content).expect("Failed to write file");
+///         println!("File encrypted successfully");
+///     }
+///     Err(err) => {
+///         eprintln!("Error occurred during encryption: {:?}", err);
+///     }
+/// }
+/// ```
 pub(crate) fn encrypt_file(plain_text: Vec<u8>, key1: &Secret<Vec<u8>>, key2: &Secret<Vec<u8>>, password: &str) -> Result<Vec<u8>, Box<dyn Error>> {
 
     let key1 = key1.expose_secret();
@@ -57,6 +102,52 @@ pub(crate) fn encrypt_file(plain_text: Vec<u8>, key1: &Secret<Vec<u8>>, key2: &S
 }
 
 
+/// Decrypts the content of a file.
+///
+/// This function decrypts the content of a file using two secret keys and a password.
+///
+/// # Arguments
+///
+/// * `cipher_text` - The encrypted content of the file to be decrypted.
+/// * `key1` - A secret key used for decryption.
+/// * `key2` - Another secret key used for decryption.
+/// * `password` - The password used for decryption.
+///
+/// # Returns
+///
+/// A `Result` containing the decrypted content of the file, or an error if decryption fails.
+///
+/// # Errors
+///
+/// Returns an error if decryption fails for any reason.
+///
+/// # Example
+///
+/// ```
+/// use your_crate::decrypt_file;
+///
+/// // Read the encrypted content of the file
+/// let encrypted_content = std::fs::read("encrypted_file.txt").expect("Failed to read file");
+///
+/// // Provide secret keys and a password
+/// let key1 = Secret::new(vec![1, 2, 3]);
+/// let key2 = Secret::new(vec![4, 5, 6]);
+/// let password = "my_password";
+///
+/// // Decrypt the content of the file
+/// let decrypted = decrypt_file(encrypted_content, &key1, &key2, password);
+///
+/// match decrypted {
+///     Ok(decrypted_content) => {
+///         // Write the decrypted content to another file
+///         std::fs::write("decrypted_file.txt", decrypted_content).expect("Failed to write file");
+///         println!("File decrypted successfully");
+///     }
+///     Err(err) => {
+///         eprintln!("Error occurred during decryption: {:?}", err);
+///     }
+/// }
+/// ```
 pub(crate) fn decrypt_file(cipher_text: Vec<u8>, key1: &Secret<Vec<u8>>, key2: &Secret<Vec<u8>>, password: &str) -> Result<Vec<u8>, Box<dyn Error>> {
 
 
